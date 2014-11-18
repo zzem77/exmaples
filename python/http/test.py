4e4f2238a9746b616f5d81ef30cdf96d96a22658
Hosup Lee <zzem77@gmail.com>
2014-11-18 18:59:09 +0900

    urllib, urllib2 test

--- python/http/test.py
+++ python/http/test.py
@@ -1,4 +1,15 @@
-import re, urllib, urllib2
+import urllib
+import urllib2
 
-url = "http://webhacking.kr/challenge/web/web-02/"
-urllib2.Request(url)
+# url = "http://webhacking.kr/challenge/web/web-02/"
+url = "http://webhacking.kr/index.html?enter=1"
+login_form = {"id": "aaa", "pw": "aaaa"}
+login_req = urllib.urlencode(login_form)
+request = urllib2.Request(url, login_req)
+response = urllib2.urlopen(request)
+
+print response.read()
+
+cookie = response.headers.get('Set-Cookie')
+
+print cookie
