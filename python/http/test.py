f236460a937a8b60f987c8d11e93bdeb469c7d1e
Hosup Lee <zzem77@gmail.com>
2014-11-18 19:12:42 +0900

    cookie 값 확인

--- python/http/test.py
+++ python/http/test.py
@@ -1,14 +1,18 @@
+# coding: utf8
 import urllib
 import urllib2
 
 # url = "http://webhacking.kr/challenge/web/web-02/"
 url = "http://webhacking.kr/index.html?enter=1"
+
+# urllib을 스는 이유 -urlencode()
 login_form = {"id": "aaa", "pw": "aaaa"}
 login_req = urllib.urlencode(login_form)
+
 request = urllib2.Request(url, login_req)
 response = urllib2.urlopen(request)
 
-print response.read()
+# print response.read()
 
 cookie = response.headers.get('Set-Cookie')
 
